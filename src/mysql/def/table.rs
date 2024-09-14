431fcbf3ef536461a121b16fc6d29fe158795add
Bora Arslan <bora.rs@pm.me>
2024-09-14 12:44:44 +0300

    fix: convert UNSIGNED BIGINT to BIGINT

--- src/mysql/def/table.rs
+++ src/mysql/def/table.rs
@@ -9,7 +9,7 @@ pub struct TableInfo {
     /// The name of the table
     pub name: String,
     pub engine: StorageEngine,
-    pub auto_increment: Option<u64>,
+    pub auto_increment: Option<i64>,
     pub char_set: CharSet,
     pub collation: Collation,
     pub comment: String,
